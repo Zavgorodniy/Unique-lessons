c58b0b3593de30a004ab84072ccecf30e250afa4
nick <Zavgorodniy-nick@gmail.com>
2016-02-18 17:01:32 +0200

    Client-Server

---
+++ src/ClientServer/Client.java
@@ -0,0 +1,40 @@
+import java.net.*;
+import java.io.*;
+
+public class Client {
+    public static void main(String[] ar) {
+        int serverPort = 6666; // здесь обязательно нужно указать порт к которому привязывается сервер.
+        String address = "192.168.88.32";
+
+        try {
+            InetAddress ipAddress = InetAddress.getByName(address); // создаем объект который отображает вышеописанный IP-адрес.
+            System.out.println("IP address " + address + " and port " + serverPort + "?");
+            Socket socket = new Socket(ipAddress, serverPort); // создаем сокет используя IP-адрес и порт сервера.
+            System.out.println("Yes! I just got hold of the program.");
+
+            // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиентом.
+            InputStream sin = socket.getInputStream();
+            OutputStream sout = socket.getOutputStream();
+
+            // Конвертируем потоки в другой тип, чтоб легче обрабатывать текстовые сообщения.
+            DataInputStream in = new DataInputStream(sin);
+            DataOutputStream out = new DataOutputStream(sout);
+
+            // Создаем поток для чтения с клавиатуры.
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            String line = null;
+            System.out.println();
+
+            while (true) {
+                line = keyboard.readLine(); // ждем пока пользователь введет что-то и нажмет кнопку Enter.
+                out.writeUTF(line); // отсылаем введенную строку текста серверу.
+                out.flush(); // заставляем поток закончить передачу данных.
+                line = in.readUTF(); // ждем пока сервер отошлет строку текста.
+                System.out.println(line);
+                System.out.println();
+            }
+        } catch (Exception x) {
+            x.printStackTrace();
+        }
+    }
+}
